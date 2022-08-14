3713e7114078e21157d49c494d7762e996c107ad
elnurra <elnurra@code.edu.az>
2022-08-14 23:22:20 +0400

    Task completed

---
+++ TaskForSingerSong/Models/Song.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskForSingerSong.Models
+{
+    class Song
+    {
+        private string _name;
+        private string _genre;
+        
+        public string Name { get 
+            {
+                return _name;
+            } 
+            set
+            {
+                if (value.Length<=100)
+                {
+                    _name = value;
+                }
+            } }
+        public string Genre {
+            get 
+            { 
+                return _genre; 
+            }
+            set
+            {
+                if (value== "Pop" && value == "Rock" && value == "Jazz" && value== "Techno")
+                {
+                    _genre = value;
+                }
+                else 
+                {
+                    Console.WriteLine("wrong input");
+                }
+            }
+                 }
+        public Singer Singer { get; set; }
+        private double _avrage = 0;
+
+        public void AddRating(int addrate) 
+        {
+            
+            
+                if (_avrage != 0)
+                {
+                    _avrage = (_avrage + addrate) / 2;
+                }
+                else
+                {
+                    _avrage = addrate;
+                }
+            }
+        public double GetAverageRating() 
+        {
+
+            return _avrage;
+        
+        }
+           
+        public void GetInfo() 
+        {
+            Console.WriteLine(_name + " " + _genre + " "+ _avrage);
+        }    
+            
+           
+            
+            
+
+        }
+        
+
+
+    }
+
---
+++ TaskForSingerSong/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using TaskForSingerSong.Models;
+
+namespace TaskForSingerSong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input name of singer: ");
+            string nameofSinger = Console.ReadLine();
+            Console.WriteLine("Input surname of singer: ");
+            string surnameofSinger = Console.ReadLine();
+            Console.WriteLine("Input age of singer: ");
+            int ageofSinger = Convert.ToInt32(Console.ReadLine());
+
+            Singer anysinger = new Singer
+            {
+                Name = nameofSinger,
+                Surname = surnameofSinger,
+                Age = ageofSinger
+            };
+
+            Console.Write("Input Singer's name song:");
+            string nameofSong = Console.ReadLine();
+            Console.Write("Input Singer's genre song:");
+            string genreofSong = Console.ReadLine();
+
+            Song anysong = new Song
+            {
+                Name = nameofSong,
+                Genre = genreofSong,
+                Singer = anysinger
+            };
+
+            int input;
+            do
+            {
+                Console.WriteLine("Choose 1 of them: \n (1) - Rate song: \n (0) - Exit");
+                input = Convert.ToInt16(Console.ReadLine());
+                if (input == 1)
+                {
+                    Console.WriteLine("Rate songs from 0 to 10: ");
+                    int rate = int.Parse(Console.ReadLine());
+                    if (rate >= 0 && rate <= 10)
+                    {
+                        anysong.AddRating(rate);
+                        Console.WriteLine("Do u want add rating for current song? \n (0) - Rate song: \n (1) - Exit  ");
+                        input = Convert.ToInt16(Console.ReadLine());
+                        if (input == 0)
+                        {
+                            if (rate >= 0 && rate <= 10)
+                            {
+                                anysong.AddRating(rate);
+                            }
+                        }
+                        
+                        
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your input is wrong: ");
+                    }
+                    
+
+
+                }
+                else
+                {
+
+                    break;
+                }
+                
+
+            } while (input == 0);
+            anysong.GetInfo();
+        }
+    }
+}
